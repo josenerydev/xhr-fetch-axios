3a30dbf8aeb494528e5c36386f96475ec75435b8
José Nery <josenerydev@gmail.com>
2020-07-20 19:17:51 -0300

    setup

---
+++ xhr.js
@@ -0,0 +1,13 @@
+const getBtn = document.getElementById('get-btn');
+const postBtn = document.getElementById('post-btn');
+
+const getData = () => {
+
+};
+
+const sendData = () => {
+
+};
+
+getBtn.addEventListener('click', getData);
+postBtn.addEventListener('click', sendData);
